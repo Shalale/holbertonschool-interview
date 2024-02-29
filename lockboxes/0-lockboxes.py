bf2377cc01285067576dfe52186e477a26f1f5a0
Shalale Mammadli <59388398+Shalale@users.noreply.github.com>
2024-02-29 14:42:41 +0400

    Add files via upload

---
+++ lockboxes/0-lockboxes.py
@@ -0,0 +1,17 @@
+opened_boxes = set()
+opened_boxes.add(0)
+
+def is_opened(index):
+    return index in opened_boxes
+
+def open_box(box_index): 
+    for i in boxes[box_index]:
+       if not i in opened_boxes:
+           opened_boxes.add(i)
+           print(opened_boxes)
+           open_box(i)
+           
+
+def canUnlockAll(boxes):
+    open_box(0)
+    print(len(boxes) == len(opened_boxes))
