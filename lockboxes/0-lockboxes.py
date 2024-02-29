3ef7788367a834b1b64710a6480567cf6bd5c3fd
Shalale Mammadli <59388398+Shalale@users.noreply.github.com>
2024-02-29 17:02:51 +0400

    Update 0-lockboxes.py

--- lockboxes/0-lockboxes.py
+++ lockboxes/0-lockboxes.py
@@ -9,14 +9,14 @@ opened_boxes.add(0)
 def is_opened(index):
     return index in opened_boxes
 
-def open_box(box_index): 
+def open_box(box_index, boxes): 
     for i in boxes[box_index]:
        if not i in opened_boxes:
            opened_boxes.add(i)
            open_box(i)
            
 def canUnlockAll(boxes):
-    open_box(0)
+    open_box(0, boxes)
     canUnlockAll = len(boxes) == len(opened_boxes)
     opened_boxes.clear()
     opened_boxes.add(0)
