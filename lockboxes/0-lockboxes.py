fae6bb6a3ab0ad50924c99b3471a063a25e0e9c1
Shalale Mammadli <59388398+Shalale@users.noreply.github.com>
2024-02-29 18:42:21 +0400

    Update 0-lockboxes.py

--- lockboxes/0-lockboxes.py
+++ lockboxes/0-lockboxes.py
@@ -8,9 +8,8 @@ def open_box(box_index, boxes, opened_boxes):
     for i in boxes[box_index]:
         if i >= len(boxes):
             continue
-        # if not isinstance(i, int):
-        #     continue
-        if type(i) != equal int:
+        if not isinstance(i, int):
+            print("I am in")
             continue
         if i not in opened_boxes:
             opened_boxes.add(i)
