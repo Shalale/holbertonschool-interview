7c23f2d7dd0aae2aec0721402579af611924e89d
Shalale Mammadli <59388398+Shalale@users.noreply.github.com>
2024-02-29 17:34:52 +0400

    Update 0-lockboxes.py

--- lockboxes/0-lockboxes.py
+++ lockboxes/0-lockboxes.py
@@ -8,7 +8,7 @@ opened_boxes.add(0)
 
 def open_box(box_index, boxes): 
     for i in boxes[box_index]:
-        if i.isnumeric():
+        if not i.isnumeric():
             continue
         if i >= len(boxes):
             continue
